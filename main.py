bd8717a96f01b9f5ad5085a25ec55e98594efdca
Park_Daniel <61256436+Park-Developer@users.noreply.github.com>
2022-01-24 03:12:51 +0900

    udp 통신 기능 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import socket
 import draw_func
 
+
 cap=cv2.VideoCapture(0)
 
 print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -13,7 +14,7 @@ robot_body=np.array([[0,480],[40,400],[600,400],[640,480]])
 
 # Camera Setting
 camera_info={
-'line_length':400, # Unit : Frame
+'line_length':400, # Unit : Frameasdasd
 'frame_width':cap.get(cv2.CAP_PROP_FRAME_WIDTH),
 'frame_height':cap.get(cv2.CAP_PROP_FRAME_HEIGHT),
 'ir_angle_limit':50, # Unit : degree
@@ -25,29 +26,42 @@ ir_info={
 }
 
 # UDP Communication(Client) Setting
-clientSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-clientSock.bind(('127.0.0.1',8080))
+CAMERA_CLI_IP=('169.254.42.20',8080) # Ethernet Camera IP
+MAIN_SER_IP=('169.254.42.23',8080) # Ethernet Main Controller IP
 
-received_data=clientSock.recv(1024)
-ir_angle=int(received_data.decode('utf-8'))
-clientSock.sendto("OK".encode(),('127.0.0.1',8080))
-if cap.isOpened():
-    while True:
-        
+camera_Sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+camera_Sock.bind(CAMERA_CLI_IP)
+camera_Sock.sendto("Camera Connection is OK".encode(),MAIN_SER_IP)
+is_udpOK=False
+check_msg="UDP Connection OK"
+received_data=camera_Sock.recv(1024)
+print(received_data.decode('utf-8'),str(received_data.decode('utf-8'))==check_msg)
 
-        ret, frame=cap.read()
-        
-        if ret:
-            ir_lineCoord= draw_func.calc_IR_line(ir_angle, camera_info)
-            draw_func.draw_IR_line(frame,ir_lineCoord,ir_info)
+if(str(received_data.decode('utf-8'))==check_msg):
+    is_udpOK=True
+    ir_angle=0 # initial angle 
+
+
+if (is_udpOK==True):
+    if cap.isOpened():
+        while True:
+            ret, frame=cap.read()
             
-            cv2.fillConvexPoly(frame,robot_body,(0,0,255)) # 외곽선 두께에 -1을 주면 내부가 색칳됨
-            cv2.putText(frame,"asd",(600,40),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0))
-            cv2.imshow("Camera ",frame);
-            if cv2.waitKey(1)!=-1:
-                break
-        else:
-            break                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            if ret:
+                # UDP Comuunication to receive angle
+                camera_Sock.sendto("Angle Req".encode(),MAIN_SER_IP)
+                received_data=camera_Sock.recv(1024)
+                ir_angle=int(received_data.decode('utf-8'))
+                ir_lineCoord= draw_func.calc_IR_line(ir_angle, camera_info)
+                draw_func.draw_IR_line(frame,ir_lineCoord,ir_info)
+                
+                cv2.fillConvexPoly(frame,robot_body,(0,0,255)) # 외곽선 두께에 -1을 주면 내부가 색칳됨
+                cv2.putText(frame,"asd",(600,40),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0))
+                cv2.imshow("Camera ",frame);
+                if cv2.waitKey(1)!=-1:
+                    break
+            else:
+                break                                                                                                                                                                                                                                                                                                                                                                                                                                                
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
